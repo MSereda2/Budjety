c7b5a4f115e7e904d27f5e9b251a3a08447c7325
Misha <sereda2510@mail.ru>
2019-12-14 16:58:54 +0300

    add input  get field and create objects for store DOM elements

--- app.js
+++ app.js
@@ -5,26 +5,50 @@ var budgetController = (function() {
 
 /*  UI CONTROLLER */
 var UIcontroller = (function() {
-    // some code
+    
+    var DOMstrings = {
+        inputType: `.add__type`,
+        inputdescription: `.add__description`,
+        inputValue: `.add__value`,
+        addBtn: `.add__btn`
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,  // either + or -
+                description: document.querySelector(DOMstrings.inputdescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+
+
+    }
 })();
 
 /*  APP CONTROLLER */
-var appController = (function(budgetCtrl, UlCtrl) {
+var appController = (function(budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function() {
         //1. Get the filed input data
-
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. Add the item to the budget controller  
-
+        console.log(DOM);
         //3. Add the item to the UI
 
         //4. Calculate the budget
 
         //5. Display budget
-        alert(`congrats itworks`)
+       
     }
 
-    document.querySelector(`.add__btn`).addEventListener(`click`, ctrlAddItem);
+    document.querySelector(DOM.addBtn).addEventListener(`click`, ctrlAddItem);
 
     document.addEventListener(`keypress`, function(event) {
         if(event.keyCode === 13) {

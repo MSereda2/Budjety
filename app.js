bb2cb5653a9b4bf492f04e7c8da338dffd3d93db
Misha <sereda2510@mail.ru>
2019-12-14 14:24:06 +0300

    done module patern

---
+++ app.js
@@ -0,0 +1,14 @@
+/*  BUDGET CONTROLLER */
+var budgetController = (function() {
+    // some code
+})();
+
+/*  UI CONTROLLER */
+var UIcontroller = (function() {
+    // some code
+})();
+
+/*  APP CONTROLLER */
+var appController = (function(budgetCtrl, UlCtrl) {
+    // some code
+})(budgetController, UIcontroller);
